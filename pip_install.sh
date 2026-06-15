aad5ecadad16d1d3a7f1712048d9d4d7be1ad247
nordeim <nordeimkuahsziyp@outlook.com>
2026-06-15 11:36:13 +0800

    Create pip_install.sh

---
+++ pip_install.sh
@@ -0,0 +1,2 @@
+pip install yfinance pandas numpy scipy plotly jinja2 requests beautifulsoup4 lxml 
+pip install --break-system-packages yfinance pandas numpy scipy plotly jinja2 requests beautifulsoup4 lxml 
