1dc96f12c9db49fb55577282e3c3393866263f27
nordeim <nordeimkuahsziyp@outlook.com>
2026-06-15 11:37:40 +0800

    Update pip_install.sh to include a new install command

--- pip_install.sh
+++ pip_install.sh
@@ -1,2 +1,3 @@
 pip install yfinance pandas numpy scipy plotly jinja2 requests beautifulsoup4 lxml 
 pip install --break-system-packages yfinance pandas numpy scipy plotly jinja2 requests beautifulsoup4 lxml 
+pip install --break-system-packages yfinance pandas numpy scipy plotly
